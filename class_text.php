5f6bf5ebe23b59f7f7464f5b905509ad1c2a3ff7
Max Kraan <max.kraan@gmail.com>
2019-05-15 13:17:24 +0200

    nieuw commitje

---
+++ class_text.php
@@ -0,0 +1,5 @@
+<?php
+    class Text {
+        var $wattext = "Dit is een regel tekst uit de class text";
+    }
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Dit is mijn eerste ervaring met Git";
+    include("./class_text.php");
+    echo "Dit is mijn eerste ervaring met Git<hr>";
+    echo "Dit is een nieuwe regel code<hr>";
+    echo "Dit is weer een nieuwe regel code<hr>";
+    echo "Er is weer een nieuwe les begonnen";
+
+    $tekst = new Text();
+
+    echo $tekst->wattext;
 ?>
